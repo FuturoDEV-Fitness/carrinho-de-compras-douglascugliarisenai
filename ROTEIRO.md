e5b69720717db3faff49ef537667c1853082ea12
douglascugliarisenai <douglas_s5@estudante.sc.senai.br>
2024-07-06 09:55:10 -0300

    fix: 🐛 M2S06-ex1-ajuste-validacoes
    ✅ Closes: M2S06-ex1

---
+++ ROTEIRO.md
@@ -0,0 +1,20 @@
+# Lab Commerce - Carrinho de compras
+
+## Cadastrar Clientes
+Foi criado a tabela clientes e desenvolvido a rota para cadastrar um novo cliente.
+
+## Cadastrar Produtos
+
+Foi criado a tabela categorias e produtos e desenvolvido a rota para cadastrar um novo produto.
+
+## Listagem de Produtos
+
+Foi implementado uma nova rota para listar todos os produtos.
+
+## Listagem de um Produto
+
+Foi implementado uma nova rota para listar apenas um produto.
+
+## Cadastro de Carrinho
+
+Foi criado a tabela orders e order_items e desenvolvido a rota para cadastrar um carrinho com itens.
--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -6,7 +6,28 @@ class ClientsController extends Database {
         try {
             const dados = request.body;
 
-            console.log(dados);
+            if (!dados.name) {
+                return response.status(400).json({ message: "O name é obrigatório" });
+            }
+
+            if (!dados.email) {
+                return response.status(400).json({ message: "O email é obrigatório" });
+            }
+
+            if (!dados.cpf) {
+                return response.status(400).json({ message: "O cpf é obrigatório" });
+            }
+
+            if (!dados.contact) {
+                return response.status(400).json({ message: "O contact é obrigatório" });
+            }
+
+            const sqlConsulta = "SELECT * FROM clients WHERE email = $1";
+            const result = await this.database.query(sqlConsulta, [dados.email]);
+            
+            if (result.rows.length > 0) {
+                return response.status(400).json({ message: "Email ja existe" });
+            } 
 
             const sql =
                 "INSERT INTO clients (name,email,cpf,contact) VALUES ($1, $2, $3, $4)";
